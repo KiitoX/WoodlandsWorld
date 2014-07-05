d63a17cbe9b10ca4b9be7d1f126c97aa9474ebe3
MCManuelLP <mcmanuellp@gmail.com>
2014-07-05 18:34:14 +0200

    resources go!

--- src/main/java/com/mcmanuellp/woodlandsworld/WoodlandsWorld.java
+++ src/main/java/com/mcmanuellp/woodlandsworld/WoodlandsWorld.java
@@ -1,15 +1,20 @@
 package com.mcmanuellp.woodlandsworld;
 
+import com.mcmanuellp.woodlandsworld.proxy.IProxy;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid="WoodlandsWorld", name="Woodlands World", version="1.7.10-1.0")
+@Mod(modid = "WoodlandsWorld", name = "Woodlands World", version = "1.7.10-1.0")
 public class WoodlandsWorld
 {
     @Mod.Instance("WoodlandsWorld")
     public static WoodlandsWorld instance;
+
+    @SidedProxy(clientSide = "com.mcmanuellp.woodlandsworld.proxy.ClientProxy", serverSide = "com.mcmanuellp.woodlandsworld.proxy.ServerProxy")
+    public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
