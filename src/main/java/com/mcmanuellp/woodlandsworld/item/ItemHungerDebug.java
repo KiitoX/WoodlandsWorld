2e84c760110f5c858fac1625eff12abb7bd31f06
MCManuelLP <mcmanuellp@gmail.com>
2014-07-21 09:43:45 +0200

    puuush

--- src/main/java/com/mcmanuellp/woodlandsworld/item/ItemHungerDebug.java
+++ src/main/java/com/mcmanuellp/woodlandsworld/item/ItemHungerDebug.java
@@ -1,10 +1,20 @@
 package com.mcmanuellp.woodlandsworld.item;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
 public class ItemHungerDebug extends ItemWW
 {
     public ItemHungerDebug()
     {
         super();
         this.setUnlocalizedName("hungerDebug");
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer entityPlayer)
+    {
+        return itemStack;//drain either 2 or 20/max saturation
     }
 }
